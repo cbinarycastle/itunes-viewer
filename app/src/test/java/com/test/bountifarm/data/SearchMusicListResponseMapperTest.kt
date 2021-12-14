e8cf716e2b5a316c842814fe43ba21a3e27e95af
cbinarycastle <cbinarycastle@gmail.com>
2021-12-14 17:05:43 +0900

    SearchMusicListResponseMapper 테스트 작성

---
+++ app/src/test/java/com/test/bountifarm/data/SearchMusicListResponseMapperTest.kt
@@ -0,0 +1,20 @@
+package com.test.bountifarm.data
+
+import com.test.bountifarm.data.mapper.SearchMusicListResponseMapper
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class SearchMusicListResponseMapperTest {
+
+    @Test
+    fun `음악 리스트 검색 응답을 올바르게 변환한다`() {
+        // given
+        val mapper = SearchMusicListResponseMapper()
+
+        // when
+        val actual = mapper.toMusic(TestData.musicData[0])
+
+        // then
+        assertEquals(TestData.musics[0], actual)
+    }
+}
