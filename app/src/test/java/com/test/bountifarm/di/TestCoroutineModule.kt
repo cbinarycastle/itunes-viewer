7da9481162010037e79bb79264a14dae0eb219d1
cbinarycastle <cbinarycastle@gmail.com>
2021-12-17 13:51:59 +0900

    dependency version 변경

--- app/src/test/java/com/test/bountifarm/di/TestCoroutineModule.kt
+++ app/src/test/java/com/test/bountifarm/di/TestCoroutineModule.kt
@@ -1,9 +1,5 @@
 package com.test.bountifarm.di
 
-import com.test.bountifarm.di.CoroutineModule
-import com.test.bountifarm.di.DefaultDispatcher
-import com.test.bountifarm.di.IoDispatcher
-import com.test.bountifarm.di.MainDispatcher
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
